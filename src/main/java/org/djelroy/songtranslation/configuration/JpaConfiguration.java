e04cc1e573a0aaccf8f97b83c690eb350f3a1afa
DJ Elroy <djelroys@gmail.com>
2018-02-21 15:27:24 -0500

    Configure JPA transaction, entity manager factory and datasource with Spring

---
+++ src/main/java/org/djelroy/songtranslation/configuration/JpaConfiguration.java
@@ -0,0 +1,74 @@
+package org.djelroy.songtranslation.configuration;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({ "org.djelroy.songtranslation.configuration" })
+@PropertySource(value = { "classpath:application.properties" })
+public class JpaConfiguration {
+
+	@Autowired
+	private Environment environment;
+	
+	@Bean
+	public DataSource dataSource() {
+		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+		dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+		dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+		dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+		
+		return dataSource;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean getEntityManagerFactoryBean() {
+		
+		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		
+		//entityManagerFactoryBean.setPersistenceUnitName("org.djelroy.songtranslation");
+		entityManagerFactoryBean.setDataSource(dataSource());
+		entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		entityManagerFactoryBean.setJpaProperties(getHibernateProperties());
+		
+		return entityManagerFactoryBean;
+	}
+
+	@Bean
+	public JpaTransactionManager getJpaTransactionManager() {
+		
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(getEntityManagerFactoryBean().getObject());
+		
+		return transactionManager;
+	}
+
+	private Properties getHibernateProperties() {
+		
+		Properties properties = new Properties();
+
+		properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+		properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+		properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+		properties.put("hibernate.current_session_context_class",
+				environment.getRequiredProperty("hibernate.current_session_context_class"));
+
+		return properties;
+	}
+}
