42e28c2234c8b023f315130e9ac18b0c485812a6
DJ Elroy <djelroys@gmail.com>
2018-03-16 21:17:12 -0400

    Add index on the title and the artist of a song

--- src/main/resources/scripts.sql
+++ src/main/resources/scripts.sql
@@ -4,7 +4,9 @@ CREATE TABLE songs (
 	song_artist VARCHAR(50) NOT NULL, 
 	song_lyrics VARCHAR(10000) NOT NULL,
 	song_language CHAR(2) DEFAULT 'en' NOT NULL,
-	PRIMARY KEY (song_id)
+	PRIMARY KEY (song_id),
+	INDEX song_title_idx (song_title),
+	INDEX song_artist_idx (song_artist)	
 ) ENGINE=InnoDB; 
 
 CREATE TABLE translations (
